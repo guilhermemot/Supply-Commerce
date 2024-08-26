19707e9068c58a2c2457a435a648badb5691e32c
Guilherme <guilhermemapurunga13@gmail.com>
2024-08-25 22:40:04 -0300

    Featured NewsCard 1.5/2

--- components/Helper/NewsCard.tsx
+++ components/Helper/NewsCard.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Image from 'next/image';
+import { CalendarIcon } from '@heroicons/react/16/solid';
 
-interface Props{
+interface Props {
     image: string;
     title: string;
+    details: string;
 }
-const NewsCard = ({image, title}: Props) => {
-  return 
+const NewsCard = ({ image, title, details }: Props) => {
+    return(
     <>
-    <div className='col-span-2'>
-        <Image className=''/>
-    </div>
-    </>;
-  
+        <div className='col-span-2 space-x-2 w-[100%] h-[200px]'>
+            <Image className='w-auto h-auto md:w-[100%] md:h-[100%] object-contain shadow-lg' src={`${image}`} alt={title} width={300} height={200} />
+        </div>
+        <div className='col-span-4'>
+            <h1 className='text-[22px] mb-[1rem] text-black capitalize'>{title}</h1>
+            <div className='flex mb-[0.4rem] items-center space-x-2 bg-4f772d'>
+                <h1 className='text-[11px] mb-[1rem] text-black opacity-2'>{details}</h1>
+            </div>
+        </div>
+    </>
+    );
 };
 
 export default NewsCard
--- components/News.tsx
+++ components/News.tsx
@@ -9,9 +9,15 @@ const News = () => {
         </h1>
         <div className='mt-[2rem]'>
             <div className='grid grid-cols-1 md:grid-cols-6 gap-[3rem] w-[80%] mx-auto'>
-                <NewsCard image='/image/creatinaMax.jpg' title='New Update news'/>
-                <NewsCard image='/image/creatinaMax.jpg' title='New Update news'/>
-                <NewsCard image='/image/creatinaMax.jpg' title='New Update news'/>
+                <NewsCard image='/image/creatinaMax.jpg' title='Melhor desempenho físico' details='A Creatina foi descoberta pelo cientista Michel Chevreul em 1832. É derivada de aminoácidos (arginina, metionina e glicina), e pode ser obtida através de duas fontes:
+
+Endógena , ou seja, produzida pelo nosso próprio organismo a partir destes aminoácidos;
+
+Exógena , através do consumo de carnes vermelhas, aves, peixes e alguns vegetais. Em 1kg de carne vermelha ou salmão encontramos aproximadamente 4.5g de creatina!
+
+Ela é armazenada principalmente na musculatura esquelética (95%); os 5% restante encontram-se distribuídos no cérebro, fígado, rim e testículos.'/>
+                <NewsCard image='/image/wheyMax.jpg' title='Reposição de proteinas diarias' details=''/>
+                <NewsCard image='/image/creatinaGrowth.jpg' title='Qualidade diretamente da fabrica' details=''/>
             </div>
         </div>
     </div>
